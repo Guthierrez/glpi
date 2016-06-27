732ac408b5596d9e7e7c05e863a908a2328d787a
diogo-leal <diogo.a.leal@hotmail.com>
2016-06-27 18:15:36 -0300

    Tabela de chamados solucionados
    Iniciando a tabela com chamados solucionados proposto no trabalho de manutenção de software.

--- front/helpdesk.public.php
+++ front/helpdesk.public.php
@@ -111,6 +111,19 @@ if (isset($_GET['create_ticket'])) {
       echo "</td></tr>";
    }
 
+   // listando os chamados solucionados
+   echo "<tr class='noHover'><td class='top'>";
+	  
+	  echo "<br><table class='tab_cadrehov'>";
+   echo "<tr class='noHover'><th><div class='relative'><span>Chamados solucionados</span>";
+	  echo "</table>\n";
+	  echo "</div></th></tr>\n";
+      
+	  // usar metodo
+	  // Reminder::showListForCentral(false);
+      echo "</td></tr>";
+   // }
+
    if (Session::haveRight("reminder_public", READ)) {
       echo "<tr class='noHover'><td class='top'>";
       Reminder::showListForCentral(false);
